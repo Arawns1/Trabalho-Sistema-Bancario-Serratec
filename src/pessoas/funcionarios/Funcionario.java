3690c5b2ff168811b58a5efd72737ec56230416d
Gabriel Damico <gabrieldamico22@gmail.com>
2023-04-17 15:31:45 -0300

    Atualizei a classe Funcionario

--- src/pessoas/funcionarios/Funcionario.java
+++ src/pessoas/funcionarios/Funcionario.java
@@ -1,56 +1,13 @@
 package pessoas.funcionarios;
 
-public abstract  class Funcionario {
-	private String nome;
-	private String cpf;
-	private double salario;
-	private String Matricula;
-	public String Cargo;
-	private int Senha;
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public String getCargo() {
-		return Cargo;
-	}
+import java.util.HashMap;
 
-	public void setCargo(String cargo) {
-		Cargo = cargo;
-	}
+public abstract class Funcionario {
+	public HashMap<String,Funcionario> listaFuncionarios = new HashMap<String, Funcionario>();
+	
+	public void addNovaConta() {}
 
-	public String getCpf() {
-		return cpf;
-	}
 	
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	
-	public double getSalario() {
-		return salario;
-	}
-	
-	public void setSalario(double salario) {
-		this.salario = salario;
-	}
-
-	public String getMatricula() {
-		return Matricula;
-	}
-
-	public void setMatricula(String matricula) {
-		Matricula = matricula;
-	}
-	public String toString() {
-		return "Nome =" this.nome + ", Cargo =" + Cargo + ", CPF =" + this.cpf + ", salario=" + this.salario
-				
-				+ ", Matricula=" + this.Matricula + "Cargo:" this.Cargo;}
-	
+}
 	
 	

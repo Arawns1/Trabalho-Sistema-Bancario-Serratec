aeb7e8e763b61358e57a1476dad22b1514b308ae
Gabriel Damico <gabrieldamico22@gmail.com>
2023-04-17 15:53:27 -0300

    Corrigi ContaPoupanca

--- src/contas/ContaPoupanca.java
+++ src/contas/ContaPoupanca.java
@@ -3,20 +3,10 @@ package contas;
 import pessoas.Pessoa;
 
 public class ContaPoupanca extends Conta{
-
-	public ContaPoupanca(int numero, Pessoa pessoa, double saldo, String tipoConta, String transacao) {
-		super(numero, pessoa, saldo, tipoConta, transacao);
-		
-	}
-
-	public static final double Taxaderendimento = 0.1;
 	
+	public final double TAXA_RENDIMENTO = 0.1;
 
-
-	
-
-
-
+	public void simularRendimento(double valorSimulado, int dias) {};
 
 	}
 	

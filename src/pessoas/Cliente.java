eac78a87c4c86482622ecfeaa5ae072dae6a8376
Gabriel Damico <gabrieldamico22@gmail.com>
2023-04-17 15:18:53 -0300

    Adicionei herança em Cliente

--- src/pessoas/Cliente.java
+++ src/pessoas/Cliente.java
@@ -1,5 +1,24 @@
 package pessoas;
 
-public class Cliente {
+import java.util.HashMap;
 
+import contas.Conta;
+
+public class Cliente extends Pessoa {
+	
+	public HashMap<String, Conta> listaCliente = new HashMap<String, Conta>();
+
+	public HashMap<String, Conta> getListaCliente() {
+		return listaCliente;
+	}
+
+	public void setListaCliente(HashMap<String, Conta> listaCliente) {
+		this.listaCliente = listaCliente;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nome=" + nome + ", cpf=" + cpf + ", senha=" + senha + ", tipo=" + tipo + "]";
+	}
+	
 }

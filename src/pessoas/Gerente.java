c5558b07138cb60bc1ba47ebf2c3e592795c234c
larissrosa <larissrosa.m@gmail.com>
2023-04-17 02:21:28 -0300

    criei a classe Gerente

---
+++ src/pessoas/Gerente.java
@@ -0,0 +1,10 @@
+package pessoas;
+// comentei o public void para descomentar junto com as outras classes.
+
+public class Gerente extends Pessoa {
+	
+	//public void gerarRelatorioNumeroContas(); {
+		
+	}
+
+//}

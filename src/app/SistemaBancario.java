913474b1d0e74376e2ef371497905e318855d79f
Gabriel Damico <gabrieldamico22@gmail.com>
2023-04-17 20:45:55 -0300

    Adicionando alguns comentários

--- src/app/SistemaBancario.java
+++ src/app/SistemaBancario.java
@@ -10,15 +10,26 @@ import pessoas.funcionarios.Presidente;
 public class SistemaBancario {
 
 	public static void main(String[] args) {
+		//Atribuimos o nome do banco de Set Banco
 		Banco.setNome("G5 bank");
+		//Escrever o nome do banco na tela
 		System.out.println(Banco.getNome());
 		
+		//Criando um novo presidente com construtor
+		//Construtor do presidente: (nome, CPF, senha, cargo)
 		Presidente presidente = new Presidente("Gabriel", "11154378900", 234532, Cargo.PRESIDENTE);
+		
+		//Definindo o presidente do banco.
         Banco.setPresidente(presidente); 
-		presidente.setNome("Gabriel");
-		presidente.setCpf("11154378900");
-		presidente.setTipo(Cargo.PRESIDENTE);
-		presidente.setSenha(234532);
+       
+        //Como criamos com o construtor não precisamos utilizar os sets abaixo.
+		
+        /*
+		 * presidente.setNome("Gabriel"); 
+		 * presidente.setCpf("11154378900");
+		 * presidente.setTipo(Cargo.PRESIDENTE); 
+		 * presidente.setSenha(234532);
+		 */
 		System.out.println(Banco.getPresidente()); 
 		
 		int numeroagencia = Banco.getNumeroAgencias();

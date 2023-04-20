a0c93f3e2d6c91470c0fc5ceda8b55feb44e066a
MarianeSMagalhaes <marianesantosmagalhaes@hotmail.com>
2023-04-19 22:02:01 -0300

    Validei a senha.

--- src/app/Menu.java
+++ src/app/Menu.java
@@ -5,54 +5,53 @@ import java.util.Scanner;
 import banco.Banco;
 import pessoas.Cliente;
 
-
-
 public class Menu {
 	Scanner sc = new Scanner(System.in);
+
 	public void Login() {
-	System.out.println("Bem vindo ao "+ Banco.getNome());
-	System.out.println("1. Cliente");
-	System.out.println("2. Painel Administrativo");
-	System.out.println("Digite uma Op��o");
-	int escolha = sc.nextInt();
-	
-	if(escolha == 1) {
-		fazerLoginCliente();
-		
+		System.out.println("Bem vindo ao " + Banco.getNome());
+		System.out.println("1. Cliente");
+		System.out.println("2. Painel Administrativo");
+		System.out.println("Digite uma Opção:");
+		int escolha = sc.nextInt();
+
+		if (escolha == 1) {
+			fazerLoginCliente();
+
+		}
+
+		else if (escolha == 2) {
+			// fazerLoginAdministrativo();
+		} else {
+			System.out.println("Opção Inválida, tente novamente!");
+			Login();
+
+		}
+
 	}
-	
-	else if(escolha==2) {
-		//fazerLoginAdministrativo();
-	}
-	else {
-		System.out.println("Op��o Inv�lida, tente novamente");
-		Login();
-		
-	}
-	
-	}
-	
+
 	public void fazerLoginCliente() {
-		System.out.println("Fa�a seu Login: ");
+		System.out.println("Faça seu Login ");
 		System.out.println("Digite seu CPF: ");
 		String cpf = sc.next();
 		System.out.println("Digite sua Senha: ");
-		int senha=sc.nextInt();
-		 if(Cliente.listaCliente.containsKey(cpf)) {
-			 System.out.println("Cpf existe");
-		 }else {
-			 System.out.println("CPF não identificado");
-		 }
-		
-		
+		int senha = sc.nextInt();
+		if (Cliente.listaCliente.containsKey(cpf)) {
+			System.out.println("Cpf existe");
+			if (Cliente.listaCliente.get(cpf).getSenha() == senha) {
+				System.out.println("Acesso Permitido!");
+
+			} else {
+				System.out.println("Senha incorreta!");
+			}
+		} else {
+			System.out.println("CPF não identificado");
+		}
+
 	}
-		
-	
-	
-	//public void fazerLoginAdministrativo(); {
-		
-	}
-	
-	//public void mostrarOpcoes (); {
-		
-	
+
+	// public void fazerLoginAdministrativo(); {
+
+}
+
+// public void mostrarOpcoes (); {
